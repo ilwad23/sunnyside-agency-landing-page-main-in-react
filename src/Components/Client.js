d532634bffe641c70edd6da48e74544cbe15f2e8
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-07-25 20:01:02 +0100

    updates

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import "../css/client.css"
-const Client = ({img, para, name, status}) => {
-    return (
-      <div className="client">
-          <img className="client_img" src={`images/image-${img}`} alt={`${img}`}/>
-        <p className="client_para">{para}</p>
-        <p className="client_name">{name}</p>
-        <p className="client_status">{status}</p>
-      </div>
-    );
-}
-
-export default Client
+import React from "react";
+import "../css/client.css";
+const Client = ({ img, para, name, status }) => {
+  return (
+    <div className="client">
+      <img className="client_img" src={`images/image-${img}`} alt={`${img}`} />
+      <p className="client_para">{para}</p>
+      <p className="client_name">{name}</p>
+      <p className="client_status">{status}</p>
+    </div>
+  );
+};
+export default Client;
