8fb606ff6dc0afa67e5f3c7fbf0f25eb08264082
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-11-22 13:39:13 +0000

    made services responsive

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "../css/services.css";
+// import ServPicD from "../images/desktop/image-transform.jpg";
+// import ServPicM from "../images/desktop/image-transform.jpg";
 
 const Service = ({ img, title, para, colorText }) => {
   return (
     <div className="service">
       <img src={`./images/${img}`} alt={img} className="service_img" />
-      <div
-        className="service_text"
-        style={{
-          color: colorText,
-        }}
-      >
-        <h2 className="service_title">{title}</h2>
-        <p className="service_para">{para}</p>
+      <div className="service_content">
+        <div
+          className="service_text"
+          style={{
+            color: colorText,
+          }}
+        >
+          <h2 className="service_title">{title}</h2>
+          <p className="service_para">{para}</p>
+        </div>
       </div>
     </div>
   );
